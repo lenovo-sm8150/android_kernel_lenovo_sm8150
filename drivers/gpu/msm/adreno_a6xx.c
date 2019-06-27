9eded36949b75cbbdf58f41e84d1fb48c6b195b1
Sushmita Susheelendra <ssusheel@codeaurora.org>
2019-06-27 10:55:28 -0600

    msm: kgsl: Only read throttle counters if GMU is enabled
    Throttle counters are only used with GMU.
    Platforms without the GMU should return early
    without reading them.
    
    Change-Id: I0df614439c0629afbc7a1ebaca279e4a1042949d
    Signed-off-by: Sushmita Susheelendra <ssusheel@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/gpu/msm/adreno_a6xx.c
+++ drivers/gpu/msm/adreno_a6xx.c
@@ -1458,6 +1458,9 @@ static int64_t a6xx_read_throttling_counters(struct adreno_device *adreno_dev)
 	if (!gmu_core_isenabled(device))
 		return 0;
 
+	if (!gmu_core_isenabled(device))
+		return 0;
+
 	for (i = 0; i < ARRAY_SIZE(counts); i++) {
 		if (!adreno_dev->gpmu_throttle_counters[i])
 			counts[i] = 0;
