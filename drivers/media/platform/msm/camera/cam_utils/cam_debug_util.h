43590e12d158071b28e32613133e789638634e1f
Yaroslav Furman <yaro330@gmail.com>
2019-07-13 13:59:28 +0300

    msm: camera: disable debug logging
    This is f-ing terrible.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/media/platform/msm/camera/cam_utils/cam_debug_util.h
+++ drivers/media/platform/msm/camera/cam_utils/cam_debug_util.h
@@ -56,6 +56,10 @@
 #define CAM_IR_LED     (1 << 27)
 #define STR_BUFFER_MAX_LENGTH  1024
 
+/* Enable debug logging from camera */
+#define CAM_DEBUG_LOGGING 0
+
+#if CAM_DEBUG_LOGGING
 /*
  *  cam_debug_log()
  *
@@ -71,6 +75,7 @@
  */
 void cam_debug_log(unsigned int module_id, const char *func, const int line,
 	const char *fmt, ...);
+#endif /* CAM_DEBUG_LOGGING */
 
 /*
  * cam_get_module_name()
@@ -92,6 +97,8 @@ const char *cam_get_module_name(unsigned int module_id);
 #define CAM_ERR(__module, fmt, args...)                            \
 	pr_err("CAM_ERR: %s: %s: %d " fmt "\n",                     \
 		cam_get_module_name(__module), __func__,  __LINE__, ##args)
+
+#if CAM_DEBUG_LOGGING
 /*
  * CAM_WARN
  * @brief    :  This Macro will print warning logs
@@ -215,4 +222,13 @@ const char *cam_get_module_name(unsigned int module_id);
 		cam_get_module_name(__module), __func__,  __LINE__, ##args)
 #endif
 
+#else /* CAM_DEBUG_LOGGING */
+#define CAM_WARN
+#define CAM_INFO
+#define CAM_INFO_RATE_LIMIT
+#define CAM_INFO_RATE_LIMIT_CUSTOM
+#define CAM_DBG
+#define CAM_ERR_RATE_LIMIT
+#define CAM_ERR_RATE_LIMIT_CUSTOM
+#endif /* CAM_DEBUG_LOGGING */
 #endif /* _CAM_DEBUG_UTIL_H_ */
