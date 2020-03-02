1b2aebfca4e2b8a3d325168d93296c982843bc20
Lucchetto <lucchetto.tie@live.com>
2020-03-02 13:11:48 +0100

    Techpack: audio: ASoC: tfa98xx: fix maybe uninitialized variable warning
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- techpack/audio/asoc/codecs/tfa9874/tfa98xx.c
+++ techpack/audio/asoc/codecs/tfa9874/tfa98xx.c
@@ -223,7 +223,7 @@ static enum tfa_error tfa98xx_write_re25(struct tfa_device *tfa, int value)
 static enum tfa_error tfa98xx_tfa_start(struct tfa98xx *tfa98xx, int next_profile, int vstep)
 {
 	enum tfa_error err;
-	ktime_t start_time, stop_time;
+	ktime_t start_time = 0, stop_time = 0;
 	u64 delta_time;
 
 	if (trace_level & 8) {
