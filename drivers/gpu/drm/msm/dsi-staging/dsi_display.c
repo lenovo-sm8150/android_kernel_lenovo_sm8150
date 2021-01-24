0200bef14728aca1ba78b2921138fdb09237524d
Art_Chen <>
2021-01-24 16:21:29 +0800

    drm: add dimlayer_bl sysfs node
    Signed-off-by: Art_Chen <>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -186,6 +186,7 @@ void dsi_rect_intersect(const struct dsi_rect *r1,
 	}
 }
 
+bool is_dimlayer_bl_enable;
 int dsi_display_set_backlight(struct drm_connector *connector,
 		void *display, u32 bl_lvl)
 {
@@ -5235,12 +5236,32 @@ static ssize_t sysfs_fod_ui_read(struct device *dev,
 	return snprintf(buf, PAGE_SIZE, "%d\n", status);
 }
 
+static ssize_t sysfs_dimlayer_bl_read(struct device *dev,
+	struct device_attribute *attr, char *buf)
+{
+    return snprintf(buf, PAGE_SIZE, "%d\n", is_dimlayer_bl_enable);
+}
+
+static ssize_t sysfs_dimlayer_bl_write(struct device *dev,
+	struct device_attribute *attr, char *buf, size_t count)
+{
+    int enabled = 0;
+    sscanf(buf, "%d", &enabled);
+    is_dimlayer_bl_enable = enabled > 0;
+    return count;
+}
+
+static DEVICE_ATTR(dimlayer_bl, 0664,
+			sysfs_dimlayer_bl_read,
+			sysfs_dimlayer_bl_write);
+
 static DEVICE_ATTR(fod_ui, 0444,
 			sysfs_fod_ui_read,
 			NULL);
 
 static struct attribute *display_fs_attrs[] = {
 	&dev_attr_fod_ui.attr,
+	&dev_attr_dimlayer_bl.attr,
 	NULL,
 };
 static struct attribute_group display_fs_attrs_group = {
