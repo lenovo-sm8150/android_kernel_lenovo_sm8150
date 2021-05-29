4435fc664d647a38b04435c35ad8ac63f1d907c1
Gawin-zippo <knoxme.zippo@gmail.com>
2021-05-29 16:31:58 +0700

    Revert "ext4: fix error code in ext4_commit_super"
    This reverts commit 5a480aeb8b4e2f488dbd802c4d1a55f97c744542.

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -4854,10 +4854,8 @@ static int ext4_commit_super(struct super_block *sb, int sync)
 	struct buffer_head *sbh = EXT4_SB(sb)->s_sbh;
 	int error = 0;
 
-	if (!sbh)
-		return -EINVAL;
-	if (block_device_ejected(sb))
-		return -ENODEV;
+	if (!sbh || block_device_ejected(sb))
+		return error;
 
 	/*
 	 * If the file system is mounted read-only, don't update the
