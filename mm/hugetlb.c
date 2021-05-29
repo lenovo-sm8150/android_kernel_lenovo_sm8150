99df6dbcadaea1b84978d40bd492b6d043260e7c
Gawin-zippo <knoxme.zippo@gmail.com>
2021-05-29 16:29:57 +0700

    Revert "mm/hugeltb: handle the error case in hugetlb_fix_reserve_counts()"
    This reverts commit 50f8c20c92c522bca9e374919a50fd68774fcd42.

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -589,20 +589,13 @@ void hugetlb_fix_reserve_counts(struct inode *inode)
 {
 	struct hugepage_subpool *spool = subpool_inode(inode);
 	long rsv_adjust;
-	bool reserved = false;
 
 	rsv_adjust = hugepage_subpool_get_pages(spool, 1);
-	if (rsv_adjust > 0) {
+	if (rsv_adjust) {
 		struct hstate *h = hstate_inode(inode);
 
-		if (!hugetlb_acct_memory(h, 1))
-			reserved = true;
-	} else if (!rsv_adjust) {
-		reserved = true;
+		hugetlb_acct_memory(h, 1);
 	}
-
-	if (!reserved)
-		pr_warn("hugetlb: Huge Page Reserved count may go negative.\n");
 }
 
 /*
