aa8417c47aca3dc8dd4704654c37c17eab956070
Lucchetto <lucchetto.tie@live.com>
2021-07-21 20:50:29 +0200

    f2fs: replace f2fs_err usages with f2fs_msg
        * In our ancient f2fs version f2fs_err doesn't exist yet
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -2024,7 +2024,8 @@ static int f2fs_quota_on(struct super_block *sb, int type, int format_id,
 
 	/* if quota sysfile exists, deny enabling quota with specific file */
 	if (f2fs_sb_has_quota_ino(F2FS_SB(sb))) {
-		f2fs_err(F2FS_SB(sb), "quota sysfile already exists");
+		f2fs_msg(F2FS_SB(sb), KERN_ERR,
+					"quota sysfile already exists");
 		return -EBUSY;
 	}
 
